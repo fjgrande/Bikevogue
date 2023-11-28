9683f1812608f6f236a4c7fabbfe0fa41d908597
Francisco Grande <fjgrandv@gmail.com>
2023-11-29 00:33:21 +0100

    Add GlobalStyle

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: ${({ theme }) => theme.typography.mainFont};
+  background-color: ${({ theme }) => theme.colors.mainBackground};
+  color: ${({ theme }) => theme.colors.light};
+  margin: 0;
+  padding: 20px;
+}
+
+h1, h2{
+  margin-top: 0;
+}
+
+ul, li {
+  list-style: none;
+  padding-left: 0;
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+button {
+  font: inherit;
+}
+`;
+export default GlobalStyle;
