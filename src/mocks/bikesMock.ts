4fa35ea8d52f72c3b882d2fff197834506b2358a
Francisco Grande <fjgrandv@gmail.com>
2023-11-29 19:17:07 +0100

    Add loadBikes Reducerd test

---
+++ src/mocks/bikesMock.ts
@@ -0,0 +1,28 @@
+import { BikesStructure } from "../store/features/bikes/types";
+
+export const bikesMocks: BikesStructure[] = [
+  {
+    _id: "6564a20f803b820996b50a00",
+    model: "Orbea Orca M31ETEAM 23",
+    image: "https://i.ibb.co/j656J8V/8.webp",
+    price: 3.299,
+    brand: "Orbea",
+    material: "Carbono",
+    modality: "Road",
+    size: "M",
+    component: "Sram Rival",
+  },
+  {
+    _id: "6564a20f803b820996b509ff",
+    model: "Orbea Orca M30 24",
+    image: "https://i.ibb.co/CKvhpYg/6.webp",
+    price: 2.599,
+    brand: "Orbea",
+    material: "Carbono",
+    modality: "Road",
+    size: "L",
+    component: "Shimano 105",
+  },
+];
+
+export default bikesMocks;
