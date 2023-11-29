a11200a20f84050d2deaad8e3eadf01c61c23e0e
Francisco Grande <fjgrandv@gmail.com>
2023-11-29 14:05:16 +0100

    Add HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import HomePageStyled from "./HomePageStyled";
+
+const HomePage = (): React.ReactElement => {
+  return (
+    <HomePageStyled>
+      <h2 className="page-title">your favorite bikes</h2>
+    </HomePageStyled>
+  );
+};
+
+export default HomePage;
