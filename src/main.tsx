f36a3d69465e3cfa3e53ef169d569f1ad4089538
Francisco Grande <fjgrandv@gmail.com>
2023-11-29 01:27:35 +0100

    Add App component in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { ThemeProvider } from "styled-components";
 import "@fontsource/lexend";
 import GlobalStyle from "./styles/GlobalStyle";
 import mainTheme from "./styles/mainTheme";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
       <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
