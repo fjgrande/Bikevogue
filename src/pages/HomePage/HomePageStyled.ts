f6ff8d3b87c4dbe10012e4ae7b6f9b1872cbefaf
Francisco Grande <fjgrandv@gmail.com>
2023-11-29 14:05:32 +0100

    Add HomePage style

---
+++ src/pages/HomePage/HomePageStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const HomePageStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  .page-title {
+    text-transform: uppercase;
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 30px;
+  }
+`;
+
+export default HomePageStyled;
