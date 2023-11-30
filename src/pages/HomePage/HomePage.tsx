4137c240193af12723f1b7f203b5e6432793d1c8
Francisco Grande <fjgrandv@gmail.com>
2023-11-30 22:34:49 +0100

    Add useBikesApi in homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,26 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import HomePageStyled from "./HomePageStyled";
 import { loadBikesActionCreator } from "../../store/features/bikes/bikesSlice";
-import bikesMocks from "../../mocks/bikesMock";
 import BikesList from "../../components/BikesList/BikesList";
+import useBikesApi from "../../hooks/useBikesApi";
 
 const HomePage = (): React.ReactElement => {
+  const bikes = useAppSelector((state) => state.bikesState.bikes);
+  const { getBikes } = useBikesApi();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(loadBikesActionCreator(bikesMocks));
-  }, [dispatch]);
+    (async () => {
+      const bikes = await getBikes();
+      dispatch(loadBikesActionCreator(bikes.bikes));
+    })();
+  }, [dispatch, getBikes]);
 
   return (
     <HomePageStyled>
       <h2 className="page-title">your favorite bikes</h2>
-      <BikesList bikes={bikesMocks} />
+      <BikesList bikes={bikes} />
     </HomePageStyled>
   );
 };
