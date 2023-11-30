78bf021ce7e24012d4ae104b7321cba4ecdf863d
Francisco Grande <fjgrandv@gmail.com>
2023-11-30 17:34:06 +0100

    Add BikesList in HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,8 +3,7 @@ import { useAppDispatch } from "../../store/hooks";
 import HomePageStyled from "./HomePageStyled";
 import { loadBikesActionCreator } from "../../store/features/bikes/bikesSlice";
 import bikesMocks from "../../mocks/bikesMock";
-import bikesData from "../../data/bikesdata";
-import BikeCard from "../../components/BikeCard/BikeCard";
+import BikesList from "../../components/BikesList/BikesList";
 
 const HomePage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
@@ -16,7 +15,7 @@ const HomePage = (): React.ReactElement => {
   return (
     <HomePageStyled>
       <h2 className="page-title">your favorite bikes</h2>
-      <BikeCard bike={bikesData} />
+      <BikesList bikes={bikesMocks} />
     </HomePageStyled>
   );
 };
