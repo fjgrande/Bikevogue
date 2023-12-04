7b9d2b35f5c184aba372ee50e39a8f7761ce2b8c
Francisco Grande <fjgrandv@gmail.com>
2023-12-05 00:26:19 +0100

    Update useBikesApi test

--- src/hooks/useBikesApi.ts
+++ src/hooks/useBikesApi.ts
@@ -1,17 +1,28 @@
 import { useCallback } from "react";
 import axios from "axios";
 import { BikesStructure } from "../store/features/bikes/types";
+import { useAppDispatch } from "../store/hooks";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../store/features/ui/uiSlice";
 
 const useBikesApi = () => {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
+  const dispatch = useAppDispatch();
+
   const getBikes = useCallback(async () => {
+    dispatch(showLoadingActionCreator());
+
     const { data: bikes } = await axios.get<{
       bikes: BikesStructure[];
     }>("/bikes");
 
+    dispatch(hideLoadingActionCreator());
+
     return bikes;
-  }, []);
+  }, [dispatch]);
 
   return { getBikes };
 };
