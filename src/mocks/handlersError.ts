1f7b25f9af516dcf001f15c5014659620bb67d00
Francisco Grande <fjgrandv@gmail.com>
2023-12-06 01:15:04 +0100

    Add ToastifyStyled test in Bikecard

---
+++ src/mocks/handlersError.ts
@@ -0,0 +1,12 @@
+import { http, HttpResponse } from "msw";
+
+const urlApi = import.meta.env.VITE_API_URL;
+
+const handlersError = [
+  http.get(`${urlApi}/bikes`, () => HttpResponse.error()),
+  http.delete(`${urlApi}/bikes/delete/6564a20f803b820996b50a00`, () =>
+    HttpResponse.error(),
+  ),
+];
+
+export default handlersError;
