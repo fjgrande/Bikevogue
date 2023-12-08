ccba3804af5d48b90aca43ca66b77bb8a00835a1
Francisco Grande <fjgrandv@gmail.com>
2023-12-08 16:28:33 +0100

    Add addBike function test

--- src/hooks/useBikesApi.test.tsx
+++ src/hooks/useBikesApi.test.tsx
@@ -2,6 +2,9 @@ import { renderHook } from "@testing-library/react";
 import bikesMocks from "../mocks/bikesMock";
 import useBikesApi from "./useBikesApi";
 import { providerWrapper } from "../testsUtils/providerWrapper";
+import { addedBikeMock, createdBikeMock } from "../mocks/addedBikeMock";
+import { server } from "../mocks/node";
+import handlersError from "../mocks/handlersError";
 
 describe("Given a useBikesApi hook", () => {
   describe("When it gets the information of bikes", () => {
@@ -20,7 +23,7 @@ describe("Given a useBikesApi hook", () => {
     });
   });
 
-  describe("When it calls his deleteBike method with an bike id", () => {
+  describe("When it calls his deleteBike function with an bike id", () => {
     test("Then it should delete the 'Orbea Orca M31ETEAM 23' bike", async () => {
       const expectedBikeId = bikesMocks[0]._id;
       const expectedResponse = {};
@@ -34,6 +37,40 @@ describe("Given a useBikesApi hook", () => {
       const response = await deleteBike(expectedBikeId);
 
       expect(response).toStrictEqual(expectedResponse);
+    });
+  });
+
+  describe("When it calls the addBike function with the data of the bike 'Super Orbea'", () => {
+    test("Then it should return the bike 'Super Orbea'", async () => {
+      const expectedNewBike = createdBikeMock;
+      const expectedBike = addedBikeMock;
+
+      const {
+        result: {
+          current: { addBike },
+        },
+      } = renderHook(() => useBikesApi(), { wrapper: providerWrapper });
+
+      const newBike = await addBike(expectedNewBike);
+
+      expect(newBike).toStrictEqual(expectedBike);
+    });
+  });
+
+  describe("When it calls its addBike function with a 'Super Orbea' and the response error", () => {
+    test("Then it should show a 'Sorry, we couldn't create your Bike!' message on a Toastify", async () => {
+      server.use(...handlersError);
+      const expectedNewBike = createdBikeMock;
+
+      const {
+        result: {
+          current: { addBike },
+        },
+      } = renderHook(() => useBikesApi(), { wrapper: providerWrapper });
+
+      const newBike = await addBike(expectedNewBike);
+
+      expect(newBike).toBeUndefined();
     });
   });
 });
--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import bikesMocks from "./bikesMock";
+import { addedBikeMock } from "./addedBikeMock";
 
 const urlApi = import.meta.env.VITE_API_URL;
 
@@ -9,6 +10,9 @@ const handlers = [
   }),
   http.delete(`${urlApi}/bikes/delete/6564a20f803b820996b50a00`, () => {
     return HttpResponse.json({});
+  }),
+  http.post(`${urlApi}/bikes/add`, () => {
+    return HttpResponse.json({ bike: addedBikeMock });
   }),
 ];
 
