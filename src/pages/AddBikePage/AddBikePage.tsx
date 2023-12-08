0bc29054bd61ca8b3470f8247f91f7477780078c
Francisco Grande <fjgrandv@gmail.com>
2023-12-08 16:48:14 +0100

    Add AddBikePage dispatch

--- src/pages/AddBikePage/AddBikePage.tsx
+++ src/pages/AddBikePage/AddBikePage.tsx
@@ -1,12 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import BikeForm from "../../components/BikeForm/BikeForm";
+import useBikesApi from "../../hooks/useBikesApi";
+import { addBikesActionCreator } from "../../store/features/bikes/bikesSlice";
+import { BikesData } from "../../store/features/bikes/types";
+import { useAppDispatch } from "../../store/hooks";
 
 import AddBikePageStyled from "./AddBikePageStyled";
 
 const AddBikePage = (): React.ReactElement => {
+  const { addBike } = useBikesApi();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = async (bikeData: BikesData) => {
+    const newBike = await addBike(bikeData);
+
+    if (newBike) {
+      dispatch(addBikesActionCreator(newBike));
+    }
+
+    navigate("/home");
+  };
+
   return (
     <AddBikePageStyled>
       <h1 className="page-title">add your favorite bikes</h1>
-      <BikeForm />
+      <BikeForm onSubmit={onSubmit} />
     </AddBikePageStyled>
   );
 };
