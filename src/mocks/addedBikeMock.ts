14310f64ccc8ec7783a63743415e063c73dbfcb7
Francisco Grande <fjgrandv@gmail.com>
2023-12-13 02:53:29 +0100

    Add test editBike method in useBikesApi

--- src/mocks/addedBikeMock.ts
+++ src/mocks/addedBikeMock.ts
@@ -11,7 +11,20 @@ export const addedBikeMock: BikesStructure = {
   size: "M",
   component: "Sram Rival",
 };
+
 export const createdBikeMock: BikesData = {
+  model: "SuperOrbea",
+  image: "https://i.ibb.co/j656J8V/8.webp",
+  price: 3.299,
+  brand: "Orbea",
+  material: "Carbono",
+  modality: "Road",
+  size: "M",
+  component: "Sram Rival",
+};
+
+export const updateBikeMock: BikesStructure = {
+  _id: "6564a20f803b820996b509ff",
   model: "SuperOrbea",
   image: "https://i.ibb.co/j656J8V/8.webp",
   price: 3.299,
