0f4cb13d44632684d340aa5bf6cc4051918b6b66
Francisco Grande <fjgrandv@gmail.com>
2023-12-13 20:17:20 +0100

    Add useEffect selected by Id in updatePage

--- src/pages/UpdateBikePage/UpdateBikePage.tsx
+++ src/pages/UpdateBikePage/UpdateBikePage.tsx
@@ -1,20 +1,34 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useBikesApi from "../../hooks/useBikesApi";
-import { useAppSelector } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import UpdateBikePageStyled from "./UpdateBikePageStyled";
 import { BikesData, BikesStructure } from "../../store/features/bikes/types";
 import BikeForm from "../../components/BikeForm/BikeForm";
+import { useEffect } from "react";
+import { loadSelectedBikeActionCreator } from "../../store/features/bikes/bikesSlice";
 
 const UpdateBikePage = (): React.ReactElement => {
   const bikeUpdate = useAppSelector((state) => state.bikesState.selectedBike);
-  const { editBike } = useBikesApi();
+  const { editBike, getMyBike } = useBikesApi();
   const navigate = useNavigate();
+  const { _id } = useParams();
+  const dispatch = useAppDispatch();
 
   const onSubmit = async (bikeUpdate: BikesData | BikesStructure) => {
     await editBike(bikeUpdate as BikesStructure);
 
     navigate("/home");
   };
+
+  useEffect(() => {
+    (async () => {
+      const bike = await getMyBike(_id as string);
+
+      dispatch(loadSelectedBikeActionCreator(bike as BikesStructure));
+
+      return bike;
+    })();
+  }, [dispatch, getMyBike, _id]);
 
   return (
     <UpdateBikePageStyled>
